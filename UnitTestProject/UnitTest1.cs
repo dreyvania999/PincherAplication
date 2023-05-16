d753da772294ac2bcb84b7b55983b9e5f5a45340
Иван Александрович Дрей <83556084+dreyvania999@users.noreply.github.com>
2023-05-16 13:24:25 +0300

    изменил тесты

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -48,22 +48,7 @@ namespace UnitTestProject
             Assert.NotNull(lis);
         }
 
-        [Fact]
-        public void TestFileExists()
-        {
-            //получаю путь к текущей директории и разбиваю его 
-            string[] strings = Directory.GetCurrentDirectory().Split('\\');
-            string filePath = "";
-            //Составляю путь к файлу
-            for (int i = 0; i < strings.Length - 5; i++)
-            {
-                filePath += strings[i] + "\\";
-            }
-            filePath += "PincherApp\\PincherApp\\Resources\\Raw\\pinchertestproject.json";
-            //Проверка на наличие файлла по адресу
-            bool fileExists = File.Exists(filePath);
-            Assert.True(fileExists);
-        }
+       
 
         [Fact]
         public void ConverterBaseItemToInformItem()
